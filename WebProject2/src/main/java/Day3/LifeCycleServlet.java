17771ff6a622c9fd3bd52a41849117ef6741af0e
yogeshkulkarni45 <153734614+yogeshkulkarni45@users.noreply.github.com>
2023-12-19 15:49:43 +0530

    Add files via upload

---
+++ WebProject2/src/main/java/Day3/LifeCycleServlet.java
@@ -0,0 +1,29 @@
+package Day3;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class LifeCycleServlet
+ */
+@WebServlet("/LifeCycleServlet")
+public class LifeCycleServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+
+
+		String MineType = "text/html";
+		response.setContentType(MineType);
+		String responseText = ""<h1 style = 
+	}
+
+}
